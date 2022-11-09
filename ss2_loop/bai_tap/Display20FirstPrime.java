a31bb4242568901d2aa91c2f2d9478feed79da83
NinhHoang304 <hoangninh3004@gmail.com>
2022-11-09 11:31:28 +0700

    fixed replace for -> while

--- ss2_loop/bai_tap/Display20FirstPrime.java
+++ ss2_loop/bai_tap/Display20FirstPrime.java
@@ -6,28 +6,26 @@ public class Display20FirstPrime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Nhập số lượng số nguyên cần in ra > 20: ");
+        System.out.println("Nhập số lượng số nguyên cần in ra: ");
         int number = Integer.parseInt(sc.nextLine());
 
         boolean flag = true;
         int count = 0;
+        int N = 2;
 
-        for (int i = 2; i < 1000; i++) {
-            if (count < number){
-                for (int j = 2; j < Math.sqrt(i); j++) {
-                    if (i %j == 0){
-                        flag = false;
-                        break;
-                    }
+        while (count < number) {
+            for (int i = 2; i <= Math.sqrt(N); i++) {
+                if (N % i == 0) {
+                    flag = false;
+                    break;
                 }
-                if (flag){
-                    System.out.print(i + " ");
-                    count++;
-                }
-                flag = true;
-            }else {
-                break;
             }
+            if (flag) {
+                count++;
+                System.out.print(N + ", ");
+            }
+            flag = true;
+            N++;
         }
     }
 }
